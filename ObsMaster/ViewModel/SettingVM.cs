d6f07bb9b420992dd517b984043ca558d8a41cc3
shaw <1939484341@qq.com>
2021-11-26 16:29:36 +0800

    声音和麦克风逻辑处理

--- ObsMaster/ViewModel/SettingVM.cs
+++ ObsMaster/ViewModel/SettingVM.cs
@@ -8,8 +8,25 @@ using UICore;
 
 namespace ObsMaster.ViewModel
 {
-    public partial class MainVM
+    public partial class MainVM : ViewModelBase
     {
-       
-    }
+		public int iOldVolume { get; set; } = 50;
+		//麦克风音量
+		private int _iMicVolume = 50;
+        public int iMicVolume
+		{
+            get { return _iMicVolume; }
+			set { Set("iMicVolume", ref _iMicVolume, value); }
+		}
+
+		//音量
+		public int iOldMicVolume { get; set; } = 50;
+		private int _iVolume = 50;
+		public int iVolume
+		{
+			get { return _iVolume; }
+			set { Set("iVolume", ref _iVolume, value); }
+		}
+
+	}
 }
