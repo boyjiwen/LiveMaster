2f26a3ba67cc7c6dec0aca61a4cb104b3ba2bee5
shaw <1939484341@qq.com>
2021-11-26 17:28:28 +0800

    直播平台和直播房间 动态绑定

--- ObsMaster/ViewModel/SettingVM.cs
+++ ObsMaster/ViewModel/SettingVM.cs
@@ -8,14 +8,14 @@ using UICore;
 
 namespace ObsMaster.ViewModel
 {
-    public partial class MainVM : ViewModelBase
-    {
+	public partial class MainVM : ViewModelBase
+	{
 		public int iOldVolume { get; set; } = 50;
 		//麦克风音量
 		private int _iMicVolume = 50;
-        public int iMicVolume
+		public int iMicVolume
 		{
-            get { return _iMicVolume; }
+			get { return _iMicVolume; }
 			set { Set("iMicVolume", ref _iMicVolume, value); }
 		}
 
@@ -42,6 +42,22 @@ namespace ObsMaster.ViewModel
 		{
 			get { return _bVolumeUnable; }
 			set { Set("bVolumeUnable", ref _bVolumeUnable, value); }
+		}
+
+		//房间号
+		private string _sRoomId = "";
+		public string sRoomId
+		{
+			get { return _sRoomId; }
+			set { Set("sRoomId", ref _sRoomId, value); }
+		}
+
+		//平台名
+		private string _sPlatformName = "";
+		public string sPlatformName
+		{
+			get { return _sPlatformName; }
+			set { Set("sPlatformName", ref _sPlatformName, value); }
 		}
 	}
 }
